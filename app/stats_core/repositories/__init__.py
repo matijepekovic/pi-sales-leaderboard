bad9b8d757766fc13a667dfc47882678008c3918
matijepekovic <122064226+matijepekovic@users.noreply.github.com>
2026-08-31 17:35:20 -0700

    Wire per-source credentials

--- app/stats_core/repositories/__init__.py
+++ app/stats_core/repositories/__init__.py
@@ -16,6 +16,7 @@ from .report_data import ReportDataRepository
 from .reps import RepRepository
 from .screens import ScreenRepository
 from .settings import SettingsRepository
+from .source_credentials import SourceCredentialRepository
 from .themes import ThemeRepository
 
 
@@ -27,6 +28,7 @@ class Repositories:
         self.meta = MetaRepository()
         self.settings = SettingsRepository()
         self.data_catalog = DataCatalogRepository()
+        self.source_credentials = SourceCredentialRepository(self.settings)
         self.report_data = ReportDataRepository(data_root)
         self.screens = ScreenRepository()
         self.display = DisplayRepository()
